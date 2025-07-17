ee97a8ba5530761b0dae2766a75b5686e42db7d7
Seren-Hughes <seren.hughes@gmail.com>
2025-07-17 22:13:49 +0100

    chore: update .gitignore, settings, requirements, and add Procfile for deployment

--- modmixx/settings.py
+++ modmixx/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['modmixx.herokuapp.com',]
+ALLOWED_HOSTS = ['modmixx.herokuapp.com', 'localhost', '127.0.0.1',]
 
 
 # Application definition
