cdb1d9cf246313b2e4ca7789bb10bb6fa2f9dcb5
Seren-Hughes <seren.hughes@gmail.com>
2025-07-19 23:22:18 +0100

    add: implement signup view to handle user registration and profile creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the modmixx home page!")
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user)  # Create a profile for the new user
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
