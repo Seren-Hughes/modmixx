a2e72ca6a1e3cba6264342f90d6caeff91a43eb9
Seren-Hughes <seren.hughes@gmail.com>
2025-07-20 21:23:01 +0100

    add: implement profile detail and edit views with login requirement

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
 def signup(request):
@@ -14,3 +16,20 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+@login_required
+def profile_detail(request):
+    return render(request, 'accounts/profile_detail.html', {'profile': request.user.profile})
+
+
+@login_required
+def profile_edit(request):
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_detail')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    return render(request, 'accounts/profile_edit.html', {'form': form})
