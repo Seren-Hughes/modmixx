f2647940b0072864348573e405675e724c83fc25
Seren-Hughes <seren.hughes@gmail.com>
2025-07-20 21:42:51 +0100

    add: update ProfileForm fields and create profile detail/edit templates and crispy forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_image', 'user_name', 'pronouns', 'bio']
+        fields = ['profile_picture', 'display_name', 'pronouns', 'bio']
