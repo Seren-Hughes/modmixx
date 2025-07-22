c912136280661d4e2c7c2189bdc957adf0c145b2
Seren-Hughes <seren.hughes@gmail.com>
2025-07-22 15:47:24 +0100

    add: update logout view to use LogoutView and enhance login template with Google login option

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html', http_method_names=['get', 'post']), name='logout'),
     path('profile/', views.profile_detail, name='profile_detail'),
     path('profile/setup/', views.profile_setup, name='profile_setup'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
