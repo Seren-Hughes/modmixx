7e5b26b5d07dfbce460139625e6977e3a17e53da
Seren-Hughes <seren.hughes@gmail.com>
2025-07-30 08:44:27 +0100

    fix: allow editing tracks without requiring re-upload of audio file; only validate new uploads for size

--- tracks/forms.py
+++ tracks/forms.py
@@ -38,10 +38,17 @@ class TrackUploadForm(forms.ModelForm):
             }),
         }
         
-    def clean_audio_file(self):
-        audio_file = self.cleaned_data.get('audio_file')
-        if audio_file:
-            # Validate file size (max 50MB) 
-            if audio_file.size > 50 * 1024 * 1024:
-                raise forms.ValidationError('Audio file too large (max 50MB)')
-        return audio_file 
+        # https://stackoverflow.com/questions/51075396/python-django-does-not-overwrite-newly-uploaded-file-with-old-one
+        # https://stackoverflow.com/questions/69448205/how-to-update-filefield-data-only-if-a-new-file-has-been-chosen-in-django-or-to
+        # https://stackoverflow.com/questions/17774636/editing-a-django-model-with-a-filefield-without-re-uploading-the-file
+        def clean_audio_file(self):
+            audio_file = self.cleaned_data.get('audio_file')
+            # Only validate if a new file is uploaded
+            if audio_file and hasattr(audio_file, 'size'):
+                if audio_file.size > 50 * 1024 * 1024:
+                    raise forms.ValidationError('Audio file too large (max 50MB)')
+                return audio_file
+            # If editing and no new file uploaded, keep the existing file
+            elif self.instance and self.instance.pk:
+                return self.instance.audio_file
+            return audio_file
