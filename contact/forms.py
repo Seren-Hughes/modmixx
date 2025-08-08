4d0cac74b434afe89ea517a0eff4c32dc431a2e3
Seren-Hughes <seren.hughes@gmail.com>
2025-08-08 21:48:57 +0100

    add: implement honeypot field in ContactForm to detect spam submissions

--- contact/forms.py
+++ contact/forms.py
@@ -6,5 +6,13 @@ class ContactForm(forms.Form):
     email = forms.EmailField(required=True)
     subject = forms.CharField(max_length=200, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+    website = forms.CharField(required=False, widget=forms.HiddenInput)  # Honeypot field to catch spam bots
+
+    def clean_website(self):
+        data = self.cleaned_data['website']
+        if data:
+            raise forms.ValidationError("Spam detected.")
+        return data
+
 
 
