c05477b152c95cb64588c6af458a3c87d49be4aa
Seren-Hughes <seren.hughes@gmail.com>
2025-08-08 19:01:34 +0100

    add: implement ContactMessage model and integrate django admin

--- contact/forms.py
+++ contact/forms.py
@@ -4,4 +4,7 @@ class ContactForm(forms.Form):
     """Form for users to contact site administrators."""
     name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(required=True)
+    subject = forms.CharField(max_length=200, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+
+
