b94b18829c9c5908c785cc03a1c1ba06e4cc081b
Seren-Hughes <seren.hughes@gmail.com>
2025-08-08 22:00:44 +0100

    add: send confirmation email to user upon successful contact form submission

--- contact/views.py
+++ contact/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 from .forms import ContactForm
+from .models import ContactMessage
 
 # create your views here.
 def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
+            ContactMessage.objects.create(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                subject=form.cleaned_data['subject'],
+                message=form.cleaned_data['message'],
+            )
             # Send email to site admin
             send_mail(
                 subject=f"Contact Us: {form.cleaned_data['name']}",
@@ -15,8 +22,15 @@ def contact(request):
                 from_email=form.cleaned_data['email'],
                 recipient_list=[settings.DEFAULT_FROM_EMAIL], # Site admin email
             )
-            # plan to send confirmation email to user to go here
-            # Generic 'we'll get back to you soon' message
+            # Send confirmation email to user
+            send_mail(
+                subject="We've received your message",
+                message="Thank you for contacting modmixx! The team will get back to you soon.",
+                from_email="modmixx <modmixx.platform@gmail.com>",
+                recipient_list=[form.cleaned_data['email']],
+                fail_silently=True,
+            )
+            # Confirmation
             return render(request, "contact/contact_success.html")
     else:
         form = ContactForm()
